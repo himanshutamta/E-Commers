049f55a709a61d9729342f478932d825c3af0440
himanshutamta <himanshutamta43w@gmail.com>
2022-12-08 15:21:45 +0530

    my commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom'
+import Products from './components/Products';
+import Product from './components/Product';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/products/:id" element={<Product />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
